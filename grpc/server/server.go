183b08c815382dbe473497c65b3d1e735c505f38
Youssef Noureddine <44925411+Yous113@users.noreply.github.com>
2024-09-24 11:49:37 +0200

    grpc

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -6,7 +6,7 @@ import (
     "net"
 
     "google.golang.org/grpc"
-    pb "https://github.com/Yous113/Distributed-systems-/tree/50a2e0f2b96f68987e18eba4241a67a373bdfb61/grpc/school" 
+    pb "github.com/Yous113/Distributed-systems-/grpc/school;school"
 )
 
 type server struct {
